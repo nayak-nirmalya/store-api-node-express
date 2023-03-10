b8f3d5b55eae3b90cc9123a37449dcea50f48d34
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-10 16:46:50 +0530

    added pagination to api

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,7 +1,7 @@
 import Product from "../models/product.js";
 import product, { Product as ProductType } from "src/models/product.js";
 
-import { Request, Response } from "express";
+import { query, Request, Response } from "express";
 
 const getAllProductsStatic = async (req: Request, res: Response) => {
   const products = await Product.find({});
@@ -14,7 +14,7 @@ const getAllProducts = async (
   req: Request<Partial<ProductType>>,
   res: Response<{ products: ProductType[]; nbHits: number }>
 ) => {
-  const { featured, company, name, rating, price, sort } = req.query;
+  const { featured, company, name, rating, price, sort, fields } = req.query;
   const queryObject: Partial<ProductType> = {};
 
   if (featured) {
@@ -34,10 +34,9 @@ const getAllProducts = async (
     queryObject.name = { $regex: name, $options: "i" } as unknown as string;
   }
 
-  console.log(queryObject);
-
   let result = Product.find(queryObject);
 
+  // sorting
   if (sort) {
     const sortList = (sort as string).split(",").join(" ");
     result = result.sort(sortList);
@@ -45,6 +44,17 @@ const getAllProducts = async (
     result = result.sort("createdAt");
   }
 
+  // fields
+  if (fields) {
+    const fieldsList = (fields as string).split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const products = await result;
 
   res.status(200).json({
