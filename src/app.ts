8e24d211bac0f6d9203ca88c35162862bb5cc4de
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-10 10:56:56 +0530

    added default error handling

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
-console.log("Hi! from App File.");
+import "dotenv/config";
+
+import express from "express";
+
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+
+// console.log(process.env.MONGO_URI);
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Hello!</h1> <a href='/api/v1/products'>Products</a>");
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connect DB
+    app.listen(PORT, () => console.log(`Server is Running on PORT: ${PORT}`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
